305064619ede62fbfa002637e2f68cdb2e9f895e
DayDreamer3d <day.dreamer.web@gmail.com>
2018-02-04 12:51:35 +0530

    updated caching strategy and fixing bugs.

--- services/registry/_impl/registry.py
+++ services/registry/_impl/registry.py
@@ -3,9 +3,9 @@ eventlet.monkey_patch()
 
 import operator
 
-from nameko import rpc
-from nameko import containers
+from nameko import containers, rpc
 import nameko_redis
+import nameko_logstash
 
 from ... import base as _base
 from ..._utils import (
@@ -27,38 +27,52 @@ config = _config.get_config(service_name)
 class RegistryService(_base.BaseService):
     name = service_name
     session = _db.DbSession(_models.DeclarativeBase)
-    redis = nameko_redis.Redis(config['WORKING_ENV'])
+    redis = nameko_redis.Redis('services')
+    log = nameko_logstash.LogstashDependency(
+        local_file=os.path.join(
+            config['LOGGING']['DIR'],
+            '{}.yml'.format(service_name)
+        )
+    )
 
     @rpc.rpc
     def add_tags(self, tags):
         tags_in_db = self.get_tags(tags)
 
         tags_to_add = list(
-            set(tags).difference([tag.name for tag in tags_in_db])
+            set(tags).difference([tag[0] for tag in tags_in_db])
         )
         _query.add_tags(self.session, tags_to_add)
 
-        # _cache.add_tags([(tag, 1) for tag in tags])
+        tags = [(tag, 1) for tag in tags]
+        _cache.add_tags(self.redis, config['CACHE']['KEY'], tags)
 
     @rpc.rpc
     def get_tags(self, tags=None):
-        result = _cache.get_tags(self.redis, config['CACHE']['KEY'], tags)
+        tags = tags or []
+        cached_tags, non_cached_tags = [], []
 
-        remaining_tags = [name for name, popularity in result if name not in tags]
+        try:
+            cached_tags, non_cached_tags = _cache.get_tags(
+                self.redis,
+                config['CACHE']['KEY'],
+                tags
+            )
+        except Exception as e:
+            # TODO: log Exception
+            pass
 
-        if remaining_tags:
-            db_result = [
-                tuple(tag.name, tag.popularity)
-                for tag in _query.get_tags(self.session, tags)
-            ]
+        if cached_tags and not non_cached_tags:
+            return cached_tags + non_cached_tags
 
-            # update the cache
-            # _cache.add_tags(db_result)
+        non_cached_tags = [
+            (tag.name, tag.popularity)
+            for tag in _query.get_tags(self.session, non_cached_tags)
+        ]
 
-            result = list(set(result).union(db_result))
-            result.sort(key=operator.itemgetter(1), reverse=True)
+        _cache.add_tags(self.redis, config['CACHE']['KEY'], non_cached_tags)
 
-        return result
+        return cached_tags + non_cached_tags
 
     @rpc.rpc
     def add_repos(self, repos):
@@ -68,90 +82,108 @@ class RegistryService(_base.BaseService):
             for repo, info in repo_obj.items():
                 tags.extend(info['tags'])
 
-        # Only update the unique tags
+        # Only update the unique and new tags
         # REVIEW: this would be a rpc call, do we want to do this way
         # or a direct query ??
         self.add_tags(list(set(tags)))
 
         # update popularity for tags (include duplicates)
-        _query.update_popularity(tags)
+        # _query.update_popularity(self.session, tags)
+
         added_repos = _query.add_repos(self.session, repos)
 
-        # if tag in cache
         if added_repos:
-            _cache.add_repos(added_repos)
-        # update tag key and repo
+            try:
+                _cache.add_repos(
+                    self.redis,
+                    config['CACHE']['KEY'],
+                    added_repos,
+                    only_update=True
+                )
+            except Exception:
+                # TODO: log exception
+                pass
 
-    # TODO: fix this service method to get the items from db
     @rpc.rpc
     def get_repos(self, tags=None):
-        # _query.update_popularity(tags)
+        # Check the labels
+        # for existent tags (cached tags) -> get the repos
+        # for non cached tags -> get it from db
+        # update the labels in cache
+        # update missing repos
+        # TODO: for now get all the repos from db
+        tags = tags or []
 
         # fetch from cache
-        cached_repos, non_cached_tags = _cache.get_repos(tags)
+        cached_repos, non_cached_tags = _cache.get_repos_from_tags(
+            self.redis,
+            config['CACHE']['KEY'],
+            tags
+        )
 
-        # fetch only non cached tag tags from db
-        db_repos = []
-        if non_cached_tags:
-            db_repos = _query.get_repos(self.session, non_cached_tags)
+        # fetch only non cached tags from db
+        db_repos = _query.get_repos_from_tags(self.session, non_cached_tags)
 
         # update cache with non cached tag results
         if db_repos:
-            _cache.add_repos(db_repos)
+            _cache.add_repos(self.redis, config['CACHE']['KEY'], db_repos)
+
+        _query.update_popularity(self.session, tags)
+
+        db_repos = [
+            {
+                'name': repo.name,
+                'description': repo.description,
+                'downloads': repo.downloads,
+                'uri': repo.uri,
+                'tags': [tag.name for tag in repo.labels]
+            }
+            for repo in db_repos
+        ]
+        repos = cached_repos + db_repos
+        self.update_downloads([repo['name'] for repo in repos])
 
         return repos
 
     @rpc.rpc
-    @_cache.update_repo_downloads([repo])
-    def update_repo_downloads(self, repo):
-        _query.update_downloads([repo])
+    def get_repo(self, repo):
+        result = {}
+
+        try:
+            result = _cache.get_repo_details(self.redis, config['CACHE']['KEY'], repo)
+        except Exception as e:
+            # TODO: log Exception
+            pass
+
+        if result:
+            return result
+
+        repo = _query.get_repo_details(self.session, repo)
+
+        # cache the result
+        _cache.add_repos(self.redis, config['CACHE']['KEY'], [repo])
+
+        return {
+            'name': repo.name,
+            'tags': [tag.name for tag in repo.labels],
+            'description': repo.description,
+            'downloads': repo.downloads,
+            'uri': repo.uri,
+        }
+
+    @rpc.rpc
+    def update_downloads(self, repos):
+        # _cache.update_downloads([repo])
+        _query.update_downloads(self.session, repos)
 
 
 def create_container():
     service_container = containers.ServiceContainer(
         RegistryService,
-        config=RegistryService.config
+        config=config
     )
     return service_container
 
-# service_container.start()
-# service_container.wait()
-
-
-    # @rpc.rpc
-    # def get_tags(self):
-    #     """ Get the tags for repositories
-    #     """
-    #
-    #     # try to get the people from cache.
-    #     try:
-    #         people = _cache.get_people(self.redis, self.repo_key)
-    #         _log.get_people(self.log, 'info', self.repo_key, people, 'cache')
-    #         return people
-    #
-    #     except Exception as e:
-    #         _log.log(self.log, 'warn', str(e).encode())
-    #
-    #         # try from db
-    #         try:
-    #             people = _orm.get_all_people(self.session, _models.Person)
-    #             _log.get_people(self.log, 'info', self.repo_key, people, 'db')
-    #
-    #             # cache missing these entries, add them to cache
-    #             try:
-    #                 _cache.add_people(self.redis, self.repo_key, people)
-    #             except Exception as e:
-    #                 _log.log(self.log, 'warn', str(e).encode())
-    #
-    #             # get values from cache
-    #             return _cache.get_people(self.redis, self.repo_key)
-    #
-    #         except Exception as e:
-    #             _log.log(self.log, 'error', str(e).encode())
-    #
-    # @rpc.rpc
-    # def get_repos(self, tags=()):
-    #     pass
 
 if __name__ == '__main__':
     service_container = create_container()
