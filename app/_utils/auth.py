4a3429b129190207f8123baaa8362ebb615594c7
DayDreamer3d <day.dreamer.web@gmail.com>
2018-02-04 12:21:52 +0530

    build the auth. for the app.

--- app/_utils/auth.py
+++ app/_utils/auth.py
@@ -10,51 +10,54 @@ config = config.get_config()
 
 def add_client_key():
     redis_server = redis.StrictRedis.from_url(config['REDIS_URIS']['app'])
-    key = config['CACHE']['DELIMITER'].join(
-        cache['CACHE']['KEY'],
+    key = config['CACHE']['DELIMITER'].join([
+        config['CACHE']['KEY'],
         'client',
-    )
-    return redis.zadd(key, uuid.uuid4().hex, redis.time()[0])
+    ])
+    name = uuid.uuid4().hex
+    value = float(redis_server.time()[0])
+    redis_server.zadd(key, value, name)
+    return name
 
 
 def validate_client_key(client_key, ttl=86400):
     redis_server = redis.StrictRedis.from_url(config['REDIS_URIS']['app'])
 
-    key = config['CACHE']['DELIMITER'].join(
-        cache['CACHE']['KEY'],
-        'client',
-    )
+    key = config['CACHE']['DELIMITER'].join([
+        config['CACHE']['KEY'],
+        'client'
+    ])
 
-    creation_time = redis.zscore(key, client_key)
+    creation_time = redis_server.zscore(key, client_key)
     if creation_time is None:
         return False
 
-    current_time = redis.time()[0]
+    current_time = redis_server.time()[0]
 
-    if creation_time + ttl < current_time:
+    if current_time - creation_time < ttl:
         return True
 
-    redis.zrm(client_key)
+    redis_server.zrem(key, client_key)
     return False
 
 
 def validate_client(func):
     @functools.wraps(func)
-    def wrapped(*args, **kwargs):
-        client_key = request.args.get('client_key')
+    def wrapper(*args, **kwargs):
+        client_key = flask.request.args.get('client-key')
         if not (client_key and validate_client_key(client_key)):
             response = {
                 'error': 'unauthorised',
                 'message': 'Unauthorised client. Please generate new Client Key.',
-                'key-generator': '/api/auth/client-key'
+                'key-generator-url': '{}/auth/client-key'.format(flask.url_for('api_home'))
             }
             return flask.make_response(
                 flask.jsonify(response),
                 401,
                 {
                     'WWW-Authenticate': 'CustomBasic realm="Authentication Required"',
-                    'Location': '/api/auth/client-key'
+                    'Location': '{}/auth/client-key'.format(flask.url_for('api_home'))
                 }
             )
         return func(*args, **kwargs)
-    return func
+    return wrapper
