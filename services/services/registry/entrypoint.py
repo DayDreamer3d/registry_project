830049538bd5d4ca411ccd505bd7fd42ef6fce82
DayDreamer3d <day.dreamer.web@gmail.com>
2018-02-11 01:53:31 +0530

    removed inserting data function, using commands instead.

--- services/services/registry/entrypoint.py
+++ services/services/registry/entrypoint.py
@@ -1,13 +1,8 @@
 """ Service entrypoint for docker compose which also act as a bootstrap for service.
 """
 
-import random
-
-import sqlalchemy
-from sqlalchemy.orm import sessionmaker
-
 from .._utils import config, db
-from ._impl import service, models
+from ._impl import service
 
 
 def create_db():
@@ -15,92 +10,6 @@ def create_db():
     """
     # create the db, sqlachemy doesn't create db
     db.create_db(service.config['DB_URIS']['registry:Base'])
-
-
-def insert_data():
-    """ Insert the initial data for the service.
-
-        NOT IN USE.
-    """
-    # TODO: use service calls to add the data
-    repos = [
-        {'alembic-base': {
-            'description': 'The base of alembic caches.',
-            'downloads': 342,
-            'uri': 'alembic-base.v1',
-            'tags': [
-                '"alembic file format"',
-                '"vfx pipeline"'
-            ]
-        }},
-        {'usd_dev:v2': {
-            'description': 'Second version of USD in Docker development.',
-            'downloads': 250,
-            'uri': 'usd_dev.v2',
-            'tags': [
-                'docker usd',
-                '"vfx pipeline"'
-            ]
-        }},
-        {'usd_dev:anim': {
-            'description': 'Production version of USD in Docker for feature animation pipeline.',
-            'downloads': 410,
-            'uri': 'usd_dev.anim',
-            'tags': [
-                'docker usd',
-            ]
-        }},
-        {'nodes editor': {
-            'description': 'Generic Qt Node editor.',
-            'downloads': 500,
-            'uri': 'nodes_editor.v1',
-            'tags': [
-                'node graph qt',
-                '"vfx pipeline"'
-            ]
-        }},
-        {'usd_standalone': {
-            'description': 'Standalone USD workflow.',
-            'downloads': 467,
-            'uri': 'usd_standalone.v1',
-            'tags': [
-                '"universal scene description"',
-                '"vfx pipeline"'
-            ]
-        }},
-    ]
-
-    tags = []
-    for repo_info in repos:
-        for repo, info in repo_info.items():
-            tags.extend(info['tags'])
-    tags = list(set(tags))
-
-    engine = sqlalchemy.create_engine(
-        service.RegistryService.config['DB_URIS']['registry:Base']
-    )
-
-    db_session = sessionmaker(bind=engine)()
-    with db.session_scope(db_session) as db_session:
-        tags = [
-            models.Tag(name=tag, popularity=random.randint(1, 10))
-            for tag in tags
-        ]
-        db_session.add_all(tags)
-
-        for repo_info in repos:
-            for repo, info in repo_info.items():
-                repo = models.Repository(
-                    name=repo,
-                    description=info['description'],
-                    downloads=info['downloads'],
-                    uri=info['uri']
-                )
-                db_session.add(repo)
-
-                for tag in info['tags']:
-                    tag = db_session.query(models.Tag).filter(models.Tag.name == tag).one()
-                    repo.labels.append(tag)
 
 
 if __name__ == '__main__':
@@ -112,8 +21,6 @@ if __name__ == '__main__':
     # starting the service will create the tables
     container.start()
 
-    # insert the data
-    # insert_data()
 
     # keep the service running
     container.wait()
