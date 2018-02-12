2bbad0743d9f458d98fc0881e460ade977b0b96e
DayDreamer3d <day.dreamer.web@gmail.com>
2018-02-12 23:24:38 +0530

    added the create db code to the entrypoint module.

--- services/services/registry/entrypoint.py
+++ services/services/registry/entrypoint.py
@@ -1,15 +1,21 @@
 """ Service entrypoint for docker compose which also act as a bootstrap for service.
 """
 
-from .._utils import config, db
+from .._utils import config
 from ._impl import service
+
+import sqlalchemy
+import sqlalchemy_utils
 
 
 def create_db():
     """ Create the database for the service on db server.
     """
     # create the db, sqlachemy doesn't create db
-    db.create_db(service.config['DB_URIS']['registry:Base'])
+    engine = sqlalchemy.create_engine(service.config['DB_URIS']['registry:Base'])
+    if sqlalchemy_utils.database_exists(engine.url):
+        sqlalchemy_utils.drop_database(engine.url)
+    sqlalchemy_utils.create_database(engine.url)
 
 
 if __name__ == '__main__':
