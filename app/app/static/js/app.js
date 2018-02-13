e892d507f2326db068d67475eedd81c5658a4318
DayDreamer3d <day.dreamer.web@gmail.com>
2018-02-13 18:01:16 +0530

    if no tags are selected don't make the ajax call.

--- app/app/static/js/app.js
+++ app/app/static/js/app.js
@@ -6,6 +6,7 @@ function attach_search_event() {
          var tags_str = '&';
 
          var checkboxes = $("input[id^='list-checkbox-']");
+         if (checkboxes.length == 0) { return null; }
          for (i = 0; i < checkboxes.length; i ++) {
              if ($(checkboxes[i]).is(':checked')) {
                  var tag_name = $(checkboxes[i]).parent().siblings("span").text();
@@ -13,14 +14,18 @@ function attach_search_event() {
              }
          }
 
+        $('#repo_cards_container').empty();
+
+        if (tags_str.length === 1) { return null; }
+
          tags_str = tags_str.slice(0, -1);
+
          var url = 'http://localhost:5000/repo_cards?client-key=' + client_key + tags_str;
          $.ajax({
              url: url,
              type: 'GET',
              dataType: 'html',
              success: function(data, status, jqXHR) {
-                 $('#repo_cards_container').empty();
                  $('#repo_cards_container').append(data);
                  attach_download_event();
              }
